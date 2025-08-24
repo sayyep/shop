6e74d02e66d86a61b7d9a114964b9d48b6150853
duong.bui <duong.bui@ntq-solution.com.vn>
2025-08-24 20:10:16 +0700

    push4
    push3

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks.1"""
+    """Run administrative tasks.3"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop.settings')
     try:
         from django.core.management import execute_from_command_line
